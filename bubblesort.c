eed00d212cea4d07d1ff1817d674ff159689bca1
dpgpmt <dpgpmt@gmail.com>
2026-02-10 14:53:23 +0530

    Program for the bubble sort

---
+++ bubblesort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int n,i,j,k,num,loca;
+            int arr[50];
+
+    printf("Enter the size of array :");
+    scanf("%d",&n);
+
+    for(i=0; i<n; i++){
+        printf("Enter the %dth element of array :",i);
+        scanf("%d",&arr[i]);
+    }
+    for(k=0; k<(n-1); k++){
+    for(j=0; j<n; j++){
+        if(arr[j]> arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+
+printf("The output is ");
+    for(i=0; i<n; i++){
+        printf("The %dth element of array :%d\n",i,arr[i]);
+    }
+}
