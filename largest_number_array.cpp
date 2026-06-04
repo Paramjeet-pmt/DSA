019e0c897e285b76bccf712c3b5ca8538b7005f8
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2026-06-04 23:12:11 +0530

    program for finding largest number from the array

--- largest_number_array.cpp
+++ largest_number_array.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 int main(){
-    int marks[] = {31,324,13,23,-8923, 2432};
+    int marks[] = {31,324,13,23,-8923};
     int largest = INT_MIN;
+    int index;
     int size = sizeof(marks)/ sizeof(int);
 
     for(int i = 0; i< size; i++){
         if(largest < marks[i]){
+            index = i;
             largest = marks[i];
         }   
     }
+    cout << "The index of largest number is " << index << endl;
     cout << largest << endl;
-    
+
 }
