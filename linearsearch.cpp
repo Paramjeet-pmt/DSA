565b5b7667caa089fd9bfa9a37441485977b4f39
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2026-06-04 23:31:24 +0530

    Program to implement linear search

---
+++ linearsearch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int search(int arr[], int size, int target){
+    for(int i = 0; i< size; i++){
+        if(arr[i] == target){
+        return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {4,2,7,8,1,2,5};
+    int size = sizeof(arr)/sizeof(int);
+    int target = 80;
+    int index = search(arr, size, target);
+    cout << index << endl;
+}
